e2035d9235dadf9d93b9f6a0d5a5b0e4dce40bfe
veltocc-boop <veltocc@gmail.com>
2026-05-04 08:58:05 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,12 @@ import app from './';
 
 const server = createServer(app);
 
-const port = config.get('app.port');
+// ✅ Use Fly port OR fallback
+const port = process.env.PORT || config.get('app.port');
+
 connectDb(config.get('db.url'))
   .then(() => {
-    server.listen(port, () => {
+    server.listen(port, '0.0.0.0', () => {
       console.log(
         `⚡ [${new Date().toTimeString()}]: Server is running on ${port}`
       );
